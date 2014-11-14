a2628dc7f4cbf12b381d8f141eb8772524b9295f
modmuss50 <m123world@gmail.com>
2014-11-14 21:14:29 +0000

    version bump

--- src/main/java/com/atlauncher/data/Constants.java
+++ src/main/java/com/atlauncher/data/Constants.java
@@ -3,7 +3,7 @@ package com.atlauncher.data;
 
 public class Constants {
     public static final String launcherName = "UntitledLauncher";
-    public static final LauncherVersion VERSION = new LauncherVersion(1, 0, 0, 0);
+    public static final LauncherVersion VERSION = new LauncherVersion(1, 0, 0, 1);
     public static final String API_BASE_URL = "";
     public static final String PASTE_CHECK_URL = "";
     public static final String PASTE_API_URL = "";
