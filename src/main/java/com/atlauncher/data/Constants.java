d605444bb99ae67ce9d8965bff3702631b2fc372
modmuss50 <m123world@gmail.com>
2014-11-19 18:28:30 +0000

    added log uploader :) thanks @Rushmead

--- src/main/java/com/atlauncher/data/Constants.java
+++ src/main/java/com/atlauncher/data/Constants.java
@@ -3,9 +3,9 @@ package com.atlauncher.data;
 
 public class Constants {
     public static final String launcherName = "OpenLauncher";
-    public static final LauncherVersion VERSION = new LauncherVersion(1, 0, 0, 3);
+    public static final LauncherVersion VERSION = new LauncherVersion(1, 0, 1, 0);
     public static final String API_BASE_URL = "";
-    public static final String PASTE_CHECK_URL = "";
-    public static final String PASTE_API_URL = "";
+    public static final String PASTE_CHECK_URL = "http://rushmead.playat.ch/stikket/";
+    public static final String PASTE_API_URL = "http://rushmead.playat.ch/stikket/api/create";
     public static final Server[] SERVERS = new Server[] { new Server("Rushmead", "rushmead.playat.ch", false, true) };
 }
