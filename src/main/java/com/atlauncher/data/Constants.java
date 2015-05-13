b408816d4d9f2309bbe1c3962c21cf82da42f769
modmuss50 <m123world@gmail.com>
2015-05-13 20:17:02 +0100

    1.1.3.0

--- src/main/java/com/atlauncher/data/Constants.java
+++ src/main/java/com/atlauncher/data/Constants.java
@@ -3,7 +3,7 @@ package com.atlauncher.data;
 
 public class Constants {
     public static final String LAUNCHER_NAME = "OpenLauncher";
-    public static LauncherVersion VERSION = new LauncherVersion(1, 1, 1, 0, 0);
+    public static LauncherVersion VERSION = new LauncherVersion(1, 1, 3, 0, 0);
     public static final String API_BASE_URL = "";
     public static final String PASTE_CHECK_URL = "http://rushmead.playat.ch/stikket/";
     public static final String PASTE_API_URL = "http://rushmead.playat.ch/stikket/api/create";
